361919f2786a706ebaca38a059070e364d0e7f52
JayeshHire <109893459+JayeshHire@users.noreply.github.com>
2025-06-10 03:24:06 +0000

    made some changes to main file

--- project/main.py
+++ project/main.py
@@ -69,8 +69,10 @@ pages_unsigned_user = {
 # if st.user: 
 if st.user.is_logged_in:
     # Handle user database entry after successful login
-    handle_user_login()
-    pg = st.navigation(pages_signed_user)
+        handle_user_login()
+        pg = st.navigation(pages_signed_user)
+    # else :
+    #     pg = st.navigation(pages_unsigned_user)
 else:
     pg = st.navigation(pages_unsigned_user)
     
@@ -78,7 +80,6 @@ else:
 pg.run()
 
 with st.sidebar:
-    # if st.user:
         if st.user.is_logged_in:
             st.button("logout",
                       on_click=st.logout)
